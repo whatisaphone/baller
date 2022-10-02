cae2381d0a8f8521aae462218bccf7592c4b2483
John Simon <john@whatisaph.one>
2022-10-01 21:48:58 -0400

    Propagate type info through == and !=

--- src/script/types.rs
+++ src/script/types.rs
@@ -3,13 +3,16 @@ use crate::{
     script::{
         ast::{Expr, ExprId, Scripto, Stmt, StmtBlock},
         ins::Variable,
-        visit::{default_visit_stmt, Visitor},
+        visit::{default_visit_expr, default_visit_stmt, Visitor},
     },
     Config,
 };
 
 pub fn spread_types(scripto: &mut Scripto, root: &mut StmtBlock, config: &Config) {
-    let mut typist = Typist { config };
+    let mut typist = Typist {
+        types: Vec::with_capacity(64),
+        config,
+    };
     typist.block(scripto, root);
 }
 
@@ -19,22 +22,62 @@ enum Type {
 }
 
 struct Typist<'a> {
+    types: Vec<Option<Type>>,
     config: &'a Config,
+}
+
+impl Typist<'_> {
+    fn get_ty(&self, id: ExprId) -> Option<Type> {
+        *self.types.get(id)?
+    }
+
+    fn set_ty(&mut self, id: ExprId, type_: Option<Type>) {
+        let type_ = match type_ {
+            Some(type_) => type_,
+            None => return,
+        };
+        if self.types.len() < id {
+            self.types.resize_with(id, || None);
+        }
+        self.types.insert(id, Some(type_));
+    }
 }
 
 impl Visitor for Typist<'_> {
     fn stmt(&mut self, script: &mut Scripto, stmt: &mut Stmt) {
         default_visit_stmt(script, stmt, self);
+
         match stmt {
             &mut Stmt::Assign(var, expr) => {
-                let ty = match find_var_type(var, self.config) {
-                    Some(ty) => ty,
-                    None => return,
-                };
+                let ty = find_var_type(var, self.config);
                 specify(script, expr, ty, self.config);
             }
             _ => {}
         }
+    }
+
+    fn expr(&mut self, script: &mut Scripto, id: ExprId) {
+        default_visit_expr(script, id, self);
+
+        match script.exprs[id] {
+            Expr::Variable(var) => {
+                self.set_ty(id, find_var_type(var, self.config));
+            }
+            Expr::Equal(lhs, rhs) | Expr::NotEqual(lhs, rhs) => {
+                let ty = unify(self.get_ty(lhs), self.get_ty(rhs));
+                specify(script, lhs, ty, self.config);
+                specify(script, rhs, ty, self.config);
+            }
+            _ => {}
+        }
+    }
+}
+
+fn unify(lhs: Option<Type>, rhs: Option<Type>) -> Option<Type> {
+    match (lhs, rhs) {
+        (lhs, None) => lhs,
+        (None, rhs) => rhs,
+        (_, _) => None,
     }
 }
 
@@ -50,7 +93,11 @@ fn find_var_type(var: Variable, config: &Config) -> Option<Type> {
     }
 }
 
-fn specify(script: &mut Scripto, id: ExprId, ty: Type, config: &Config) {
+fn specify(script: &mut Scripto, id: ExprId, ty: Option<Type>, config: &Config) {
+    let ty = match ty {
+        Some(ty) => ty,
+        None => return,
+    };
     match (ty, &script.exprs[id]) {
         (Type::Enum(enum_id), &Expr::Number(number)) => {
             if !config.enums[enum_id].values.contains_key(&number) {
