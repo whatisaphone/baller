dadc95e313b0459675e4bcc4c5a2c6e8d05b709f
whatisaphone <hi@whatisaph.one>
2024-07-02 12:00:00 +0000

    Try to accurately preserve extra bytes in original BMAP encoding

--- src/rmim_encode.zig
+++ src/rmim_encode.zig
@@ -32,10 +32,10 @@ pub fn encode(bmp_raw: []const u8, out: anytype, fixups: *std.ArrayList(Fixup)) 
         return error.BadData;
 
     const width: u32 = @intCast(info_header.biWidth);
-    const height: u32, const top_down = if (info_header.biHeight < 0)
-        .{ @intCast(-info_header.biHeight), true }
-    else
-        .{ @intCast(info_header.biHeight), false };
+    const top_down = info_header.biHeight < 0;
+
+    if (width & 3 != 0)
+        return error.BadData; // TODO: handle stride
 
     const rmih_fixup = try beginBlock(out, "RMIH");
     try out.writer().writeInt(u16, 0, .little);
@@ -46,35 +46,22 @@ pub fn encode(bmp_raw: []const u8, out: anytype, fixups: *std.ArrayList(Fixup)) 
     const bmap_fixup = try beginBlock(out, "BMAP");
 
     try out.writer().writeByte(BMCOMP_NMAJMIN_H8);
-    try compressBmap(width, height, top_down, bmp_raw[file_header.bfOffBits..], out.writer());
+    try compressBmap(width, top_down, bmp_raw[file_header.bfOffBits..], out.writer());
 
     try endBlock(out, fixups, bmap_fixup);
 
     try endBlock(out, fixups, im00_fixup);
 }
 
-fn compressBmap(
-    width: u32,
-    height: u32,
-    top_down: bool,
-    pixels: []const u8,
-    writer: anytype,
-) !void {
+fn compressBmap(width: u32, top_down: bool, pixels: []const u8, writer: anytype) !void {
     var out = std.io.bitWriter(.little, writer);
 
-    var i = if (top_down) 0 else width * (height - 1);
-    const feed = if (top_down) 0 else width * 2;
+    var pixit = PixelIter.init(width, top_down, pixels);
 
-    var current = pixels[i];
+    var current = pixit.next() orelse return error.EndOfStream;
     try out.writeBits(current, 8);
-    i += 1;
 
-    var x = width - 1;
-    var y = height;
-
-    while (true) {
-        const pixel = pixels[i];
-
+    while (pixit.next()) |pixel| {
         const diff = @as(i16, pixel) - current;
         if (diff == 0) {
             try out.writeBits(@as(u1, 0), 1);
@@ -90,20 +77,68 @@ fn compressBmap(
         }
 
         current = pixel;
-
-        i += 1;
-        x -= 1;
-        if (x == 0) {
-            y -= 1;
-            if (y == 0)
-                break;
-            x = width;
-            i -= feed;
-        }
     }
 
     try out.flushBits();
 }
+
+const PixelIter = union(enum) {
+    top_down: struct {
+        pixels: []const u8,
+        i: u32,
+    },
+    bottom_up: struct {
+        pixels: []const u8,
+        i: u32,
+        width: u32,
+        x: u32,
+    },
+
+    fn init(width: u32, top_down: bool, pixels: []const u8) PixelIter {
+        if (top_down)
+            return .{ .top_down = .{
+                .pixels = pixels,
+                .i = 0,
+            } };
+
+        const i: u32 = @intCast(pixels.len - 1);
+        return .{ .bottom_up = .{
+            .pixels = pixels,
+            .width = width,
+            .i = i,
+            .x = width - i % width,
+        } };
+    }
+
+    fn next(self: *PixelIter) ?u8 {
+        switch (self.*) {
+            .top_down => |*s| {
+                const result = s.pixels[s.i];
+
+                s.i += 1;
+                if (s.i >= s.pixels.len)
+                    return null;
+
+                return result;
+            },
+            .bottom_up => |*s| {
+                const result = s.pixels[s.i];
+
+                s.i += 1;
+                s.x -= 1;
+                if (s.x == 0) {
+                    const feed = s.width * 2;
+                    if (s.i < feed)
+                        return null;
+                    s.i -= feed;
+                    s.x = s.width;
+                }
+
+                return result;
+            },
+        }
+    }
+};
 
 const bitmap_file_header_size = 14;
 const bitmap_info_header_size = 40;
--- src/tests.zig
+++ src/tests.zig
@@ -4,7 +4,7 @@ const build = @import("build.zig");
 const extract = @import("extract.zig");
 
 // Extract and rebuild, and verify the output is identical to the original.
-test "round trip" {
+test "round trip raw" {
     const allocator = std.testing.allocator;
 
     const extract_dir = "/tmp/baller-test-baseball-2001-extract";
@@ -14,6 +14,37 @@ test "round trip" {
         .input_path = "src/fixtures/baseball2001/baseball 2001.he0",
         .output_path = extract_dir,
         .raw = true,
+    });
+
+    try build.run(allocator, &.{
+        .project_txt_path = extract_dir ++ "/project.txt",
+        .output_path = build_dir ++ "/baseball 2001.he0",
+    });
+
+    try expectFilesEqual(
+        "src/fixtures/baseball2001/baseball 2001.he0",
+        build_dir ++ "/baseball 2001.he0",
+    );
+    try expectFilesEqual(
+        "src/fixtures/baseball2001/baseball 2001.(a)",
+        build_dir ++ "/baseball 2001.(a)",
+    );
+    try expectFilesEqual(
+        "src/fixtures/baseball2001/baseball 2001.(b)",
+        build_dir ++ "/baseball 2001.(b)",
+    );
+}
+
+test "round trip decode/encode" {
+    const allocator = std.testing.allocator;
+
+    const extract_dir = "/tmp/baller-test-baseball-2001-extract";
+    const build_dir = "/tmp/baller-test-baseball-2001-build";
+
+    try extract.run(allocator, &.{
+        .input_path = "src/fixtures/baseball2001/baseball 2001.he0",
+        .output_path = extract_dir,
+        .raw = false,
     });
 
     try build.run(allocator, &.{
