c00cc676dac506a43c05ecf46f4c5c3d9300a180
whatisaphone <hi@whatisaph.one>
2024-08-07 12:00:00 +0000

    Support symbols for local scripts

--- src/Symbols.zig
+++ src/Symbols.zig
@@ -5,6 +5,9 @@ const utils = @import("utils.zig");
 
 const Symbols = @This();
 
+const first_room = 1;
+const first_local_script = 2048;
+
 pub const ScriptId = union(enum) {
     global: u32,
     local: struct { room: u8, number: u32 },
@@ -20,14 +23,37 @@ pub const Script = struct {
     }
 };
 
+const Room = struct {
+    scripts: ArrayMap(Script) = .{},
+
+    fn deinit(self: *Room, allocator: std.mem.Allocator) void {
+        var i = self.scripts.len();
+        while (i > 0) {
+            i -= 1;
+            const script = self.scripts.getPtr(i) orelse continue;
+            script.deinit(allocator);
+        }
+        self.scripts.deinit(allocator);
+    }
+};
+
 /// Lookup table from number to name
 globals: ArrayMap([]const u8) = .{},
 /// Map from name to number
 global_names: std.StringArrayHashMapUnmanaged(u16) = .{},
 scripts: ArrayMap(Script) = .{},
+rooms: ArrayMap(Room) = .{},
 
 pub fn deinit(self: *Symbols, allocator: std.mem.Allocator) void {
-    var i = self.scripts.len();
+    var i = self.rooms.len();
+    while (i > 0) {
+        i -= 1;
+        const room = self.rooms.getPtr(i) orelse continue;
+        room.deinit(allocator);
+    }
+    self.rooms.deinit(allocator);
+
+    i = self.scripts.len();
     while (i > 0) {
         i -= 1;
         const script = self.scripts.getPtr(i) orelse continue;
@@ -85,7 +111,9 @@ fn parseLine(allocator: std.mem.Allocator, line: []const u8, result: *Symbols) !
     if (std.mem.eql(u8, part, "global"))
         try handleGlobal(&cx)
     else if (std.mem.eql(u8, part, "script"))
-        try handleScript(&cx)
+        try handleGlobalScript(&cx)
+    else if (std.mem.eql(u8, part, "room"))
+        try handleRoom(&cx)
     else
         return error.BadData;
 }
@@ -104,7 +132,7 @@ fn handleGlobal(cx: *Cx) !void {
     entry.value_ptr.* = number;
 }
 
-fn handleScript(cx: *Cx) !void {
+fn handleGlobalScript(cx: *Cx) !void {
     const number_str = cx.key_parts.next() orelse return error.BadData;
     const number = try std.fmt.parseInt(u16, number_str, 10);
 
@@ -113,11 +141,16 @@ fn handleScript(cx: *Cx) !void {
         script_entry.* = .{};
     const script = &script_entry.*.?;
 
-    const part_opt = cx.key_parts.next();
-    const part = part_opt orelse
+    try handleScript(cx, script);
+}
+
+fn handleScript(cx: *Cx, script: *Script) !void {
+    const part = cx.key_parts.next() orelse
         return handleScriptName(cx, script);
     if (std.mem.eql(u8, part, "local"))
-        return handleScriptLocal(cx, script);
+        return handleScriptLocal(cx, script)
+    else
+        return error.BadData;
 }
 
 fn handleScriptName(cx: *Cx, script: *Script) !void {
@@ -135,9 +168,43 @@ fn handleScriptLocal(cx: *Cx, script: *Script) !void {
     try script.locals.putNew(cx.allocator, number, cx.value);
 }
 
+fn handleRoom(cx: *Cx) !void {
+    const number_str = cx.key_parts.next() orelse return error.BadData;
+    const number = try std.fmt.parseInt(u8, number_str, 10);
+
+    const index = number - first_room;
+    const room_entry = try cx.result.rooms.getOrPut(cx.allocator, index);
+    if (room_entry.* == null)
+        room_entry.* = .{};
+    const room = &room_entry.*.?;
+
+    const part = cx.key_parts.next() orelse
+        return error.BadData;
+    if (std.mem.eql(u8, part, "script"))
+        try handleRoomScript(cx, room)
+    else
+        return error.BadData;
+}
+
+fn handleRoomScript(cx: *Cx, room: *Room) !void {
+    const number_str = cx.key_parts.next() orelse return error.BadData;
+    const number = try std.fmt.parseInt(u16, number_str, 10);
+
+    const index = number - first_local_script;
+    const script_entry = try room.scripts.getOrPut(cx.allocator, index);
+    if (script_entry.* == null)
+        script_entry.* = .{};
+    const script = &script_entry.*.?;
+
+    try handleScript(cx, script);
+}
+
 pub fn getScript(self: *const Symbols, id: ScriptId) ?*const Script {
     return switch (id) {
         .global => |num| self.scripts.getPtr(num),
-        .local => |_| null,
+        .local => |s| {
+            const room = self.rooms.get(s.room - first_room) orelse return null;
+            return room.scripts.getPtr(s.number - first_local_script);
+        },
     };
 }
