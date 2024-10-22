db9feb5160392fccafa9f4a4923801969409ebe6
whatisaphone <hi@whatisaph.one>
2024-10-22 12:00:00 +0000

    v0.4.2

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const extract = @import("extract.zig");
 const talkie_build = @import("talkie_build.zig");
 const talkie_extract = @import("talkie_extract.zig");
 
-const version = "0.4.1";
+const version = "0.4.2";
 
 pub fn main() !u8 {
     runCli() catch |err| {
