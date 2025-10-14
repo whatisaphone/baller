4ac7c85884186c9e20d6c1536a29c2d2b381ae75
whatisaphone <hi@whatisaph.one>
2025-10-14 19:01:11 -0400

    Clean up build.zig

--- build.zig
+++ build.zig
@@ -2,21 +2,18 @@ const std = @import("std");
 
 const version = "0.6.2";
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
     const valgrind = b.option(bool, "valgrind", "Add valgrind support") orelse false;
+
+    const optimize = b.standardOptimizeOption(.{});
 
     var target_args: std.Build.StandardTargetOptionsArgs = .{};
     if (valgrind)
         target_args.default_target.cpu_model = .baseline;
     const target = b.standardTargetOptions(target_args);
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = b.standardOptimizeOption(.{});
+    /////////
+    // exe
 
     const exe = b.addExecutable(.{
         .name = "baller",
@@ -33,33 +30,19 @@ pub fn build(b: *std.Build) void {
     exe_options.addOption([]const u8, "version", "dev");
     exe.root_module.addOptions("build_options", exe_options);
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    ///////////
+    // tests
 
     const exe_unit_tests = b.addTest(.{
         .root_module = b.createModule(.{
@@ -76,9 +59,6 @@ pub fn build(b: *std.Build) void {
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     run_exe_unit_tests.has_side_effects = true;
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 
@@ -95,6 +75,9 @@ pub fn build(b: *std.Build) void {
         test_valgrind_step.dependOn(&run_tests_valgrind.step)
     else
         test_valgrind_step.dependOn(&b.addFail("missing valgrind flag").step);
+
+    /////////////
+    // release
 
     const release = b.step("release", "Prepare release builds for all supported platforms");
     const release_targets = [_]std.Target.Query{
